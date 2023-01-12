0c57233c693b0d63c01f5f031b632036c40be943
AnyBaghdasaryan <90380024+AnyBaghdasaryan@users.noreply.github.com>
2023-01-12 22:43:05 +0400

    Create ISlide.h

---
+++ ISlide.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "IElem.h"
+
+class ISlide
+{
+public:
+	virtual void addElem(int p, IElem* t) = 0;
+	virtual void removeElem(int index) = 0;
+};
+
+class Slide : public ISlide
+{
+public:
+	void addElem(int p, IElem* t) override 
+	{
+	
+	}
+	void removeElem(int index)
+	{
+		elems.erase(elems.begin() + index);
+	}
+private:
+	std::vector<IElem> elems;
+};
