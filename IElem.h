6478bf2bb715d9704c05459e5266a575004cc7e4
AnyBaghdasaryan <90380024+AnyBaghdasaryan@users.noreply.github.com>
2023-01-12 22:45:29 +0400

    Create IElem.h

---
+++ IElem.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+
+class IElem
+{
+public:
+	virtual void print(std::ostream&) = 0;
+};
+
+
+class Rect : public IElem
+{
+private:
+	int x, y, w, l;
+
+public:
+	void print(std::ostream& output) override
+	{
+		output << "Rectangle is printed in (" << x << "," << y << "), which width is " << w << " and length is " << l << "\n";
+	}
+};
+
+class Circle : public IElem
+{
+private:
+	int x, y, r;
+
+public:
+	void print(std::ostream& output) override
+	{
+		output << "Circle is printed in (" << x << "," << y << "), which radius is equal to " << r << "\n";
+	}
+};
+
+class Line : public IElem
+{
+private:
+	int x, y, len;
+
+public:
+	void print(std::ostream& output) override
+	{
+		output << "Line is printed in (" << x << "," << y << "), which length is " << len << "\n";
+	}
+};
